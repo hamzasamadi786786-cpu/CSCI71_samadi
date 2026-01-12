9f7b5278c5eb33aa05c5f7e0715c9ad1bd3d5785
hamzasamadi786786-cpu <hamzasamadi786786@gmail.com>
2026-01-12 11:44:45 -0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: Hamza Samadi
+// Section: csci 271/0955
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
